638b2a6a5ba3011f3080effd72c834540c4f46a9
jhsilvaz13 <jhsilvaz@unal.edu.co>
2024-05-19 00:53:25 -0500

    Add check get place by name

--- places/places/views.py
+++ places/places/views.py
@@ -18,3 +18,11 @@ def PlaceCreate(request):
         variable.name = data_json["name"]
         variable.save()
         return HttpResponse("successfully created variable")
+    
+def check(request):
+    name=request.GET.get('name')
+    place = Place.objects.filter(name=name)
+    if place.exists():
+        return HttpResponse("Place exists")
+    else:
+        return HttpResponse("Place does not exist")
